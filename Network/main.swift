ec52cf2b2cbd5dd84baa2441904e34ab5c4dec46
Ruslan Kavetsky <r.kavetsky@dodopizza.com>
2020-02-24 19:26:52 +0300

    Initial Commit

---
+++ Network/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Network
+//
+//  Created by Ruslan Kavetsky on 24.02.2020.
+//  Copyright © 2020 Ruslan Kavetsky. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
